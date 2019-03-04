a31c7a4578ac8c0887d91f47d46aac51d1aca5e1
Valentin Stepanov <mail@v-stepanov.ru>
2019-03-04 14:39:41 +0500

    fix migrate

--- migrations/m190303_200722_create_user_table.php
+++ migrations/m190303_200722_create_user_table.php
@@ -14,12 +14,12 @@ class m190303_200722_create_user_table extends Migration
     {
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
-            'username' => $this->string(1024)->notNull()->unique(),
-            'password_hash' => $this->string(1024)->notNull(),
-            'password_reset_token' => $this->string(1024),
-            'email' => $this->string(1024)->unique()->notNull(),
-            'auth_key' => $this->string(1024),
-            'status' => $this->integer()->defaultValue(10),
+            'username' => $this->string(32)->notNull()->unique(),
+            'password_hash' => $this->string(64)->notNull(),
+            'password_reset_token' => $this->string(64),
+            'email' => $this->string(128)->unique()->notNull(),
+            'auth_key' => $this->string(64),
+            'status' => $this->integer(3)->defaultValue(10),
             'created_at' => $this->integer(),
             'updated_at' => $this->integer(),
         ]);
