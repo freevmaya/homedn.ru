6460f3f66aca04d40dce7caf2c6029c6d7fe47a0
Valentin Stepanov <mail@v-stepanov.ru>
2019-05-19 01:34:38 +0500

    fix

--- migrations/m190519_061046_update_data.php
+++ migrations/m190519_061046_update_data.php
@@ -3,9 +3,9 @@
 use yii\db\Migration;
 
 /**
- * Class m190304_061046_update_data
+ * Class m190519_061046_update_data
  */
-class m190304_061046_update_data extends Migration
+class m190519_061046_update_data extends Migration
 {
 
     /**
@@ -13,7 +13,8 @@ class m190304_061046_update_data extends Migration
      */
     public function safeUp ()
     {
-        $this->execute('TRUNCATE TABLE `yii_user`; INSERT INTO `yii_user` (`id`, `username`, `password_hash`, `password_reset_token`, `email`, `auth_key`, `status`, `created_at`, `updated_at`) VALUES (1, \'hradmin\', \'$2y$13$NjcNM7nsLUfhVrWK7ysEz.bGHW4RvrTWItDCRcfpi.F9L6OSQJaIW\', NULL, \'mail@v-stepanov.ru\', \'hxVGfHXcxef91MKJ1ltmnUb0_dDV_FQn\', 10, 1551645976, 1551645976);');
+        $sql = file_get_contents(__DIR__ . '/data.sql');
+        $this->execute($sql);
     }
 
     /**
@@ -21,7 +22,7 @@ class m190304_061046_update_data extends Migration
      */
     public function safeDown ()
     {
-        echo "m190304_061046_update_data cannot be reverted.\n";
+        echo "m190519_061046_update_data cannot be reverted.\n";
 
         return false;
     }
@@ -35,7 +36,7 @@ class m190304_061046_update_data extends Migration
 
       public function down()
       {
-      echo "m190304_061046_update_data cannot be reverted.\n";
+      echo "m190519_061046_update_data cannot be reverted.\n";
 
       return false;
       }
