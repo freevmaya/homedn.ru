9eebc6ab624eaf041cad9830b1e60b8f09a8c241
Valentin Stepanov <mail@v-stepanov.ru>
2019-05-20 07:29:23 +0500

    404

--- views/site/error.php
+++ views/site/error.php
@@ -1,27 +1,20 @@
 <?php
-
 /* @var $this yii\web\View */
 /* @var $name string */
 /* @var $message string */
 /* @var $exception Exception */
 
 use yii\helpers\Html;
+use app\assets\TemplateAsset;
+
+TemplateAsset::register($this);
 
 $this->title = $name;
 ?>
-<div class="site-error">
+<section class="section-1">
+    <div class="wrapper">
+        <h1><?= Html::encode($message) ?></h1>
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
     </div>
 
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
-</div>
+</section>
