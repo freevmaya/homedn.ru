4a68b52e89605e3dcb064f028331ab0183c215aa
Valentin Stepanov <mail@v-stepanov.ru>
2019-06-27 10:52:36 +0500

    portfolio bug

--- assets/js/page-portfolio.js
+++ assets/js/page-portfolio.js
@@ -1,6 +1,6 @@
 $(function () {
     $('.section-element-list .more a').click(function () {
-        $(this).parents('.section-element-list').find('ul li').fadeIn('fast');
+        $(this).parents('.section-element-list').find('ul li').fadeIn('fast').css('display', 'inline-block');
         $(this).parent().hide();
         return false;
     });
